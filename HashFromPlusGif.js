e0648113d9068d0803dccd6beca1c4c7d34ef97b
Anwesh43 <anweshthecool0@gmail.com>
2018-10-21 22:14:18 +0530

    adding State class

---
+++ HashFromPlusGif.js
@@ -0,0 +1,23 @@
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb : Function) {
+        this.scale += 0.05 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating() {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+        }
+    }
+}
